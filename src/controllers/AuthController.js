0462d6b8e198ad46b7d446b78eaafbfcbb180614
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-18 12:17:25 -0300

    Criando o auth controller

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+
+module.exports = {
+  async register(req, res) {
+    const { name, email, password } = req.body;
+
+    try {
+      if (await User.findOne({ email })) {
+        return res.status(400).json({ error: "Usuário ja existe" });
+      }
+
+      // captura do IP para modulo de segurança
+      const userIp =
+        req.ip || req.header["x-forwarded-for"] || req.socket.remoteAddress;
+
+      // criação do usuário
+      const user = User.create({
+        name,
+        email,
+        password,
+        lastIp: userIp,
+      });
+
+      return res.status(201).json({
+        message: "Usuário cadastrado!",
+        user,
+      });
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ error: `Falha ao registrar: ${e.message}` });
+    }
+  },
+};
